b011d8054289dfa2e3673828c848ce60a87b94d7
Austin Weaver <aw3335@desales.edu>
2022-09-13 21:17:02 -0400

    Added Space to Continue with Video

--- app/src/main/java/com/hfad/beeradviser/MainActivity.java
+++ app/src/main/java/com/hfad/beeradviser/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
                 String color = spinnerBeerColors.getSelectedItem().toString();
                 tvBrands.setText(getBeers(color));
-
             }
         });
     }
